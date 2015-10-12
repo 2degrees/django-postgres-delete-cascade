d282530213d5af3ef6e2c08f782673b1989db4d2
Gustavo Narea <gustavonarea@2degreesnetwork.com>
2015-10-12 14:24:47 +0100

    Prepared for first release

--- README.rst
+++ README.rst
@@ -1,19 +1,21 @@
-PostgreSQL/psycopg2 database adapter for Django that supports "ON DELETE CASCADE" at the database level
-=======================================================================================================
+PostgreSQL engine for Django that supports "ON DELETE CASCADE" at the database level
+====================================================================================
 
 This is a temporary workaround until `Django's ticket #21961
-<https://code.djangoproject.com/ticket/21961>`_ is fixed.
+<https://code.djangoproject.com/ticket/21961>`_ is fixed. It has been tested
+with Python 2.7, Django 1.6 and Psycopg2 2.6, but should work with other
+combinations.
 
 To use it, you just have to:
 
-#. Add this distribution as a dependency. For example,
+#. Install this distribution as a dependency. For example,
 
     .. code-block:: bash
 
         pip install django-postgres-delete-cascade
 
 #. Replace the ``django.db.backends.postgresql_psycopg2`` engine with
-   ``django_postgres_delete_cascade``; e.g.:
+   ``django_postgres_delete_cascade``. For example:
 
     .. code-block:: python
 
@@ -28,4 +30,11 @@ To use it, you just have to:
                 }
             }
 
-That's it!
+
+Change log
+----------
+
+Version 1.0 Release Candidate 2 (2015-10-12)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Initial release, in spite of the version (due to some problems with PYPI).
--- setup.py
+++ setup.py
@@ -25,24 +25,30 @@ _PARENT_DIRECTORY_PATH = os.path.abspath(os.path.dirname(__file__))
 
 def _read_file(file_name):
     file_ = open(os.path.join(_PARENT_DIRECTORY_PATH, file_name))
-    file_body = file_.readline().strip()
+    file_body = file_.read().strip()
     return file_body
 
 
 setup(
-    name="django-postgres-delete-cascade",
-    version=_read_file("VERSION.txt"),
-    description="",
-    long_description=_read_file("README.rst"),
-    classifiers=[],
-    keywords="",
-    author="2degrees Limited",
-    author_email="2degrees-floss@googlegroups.com",
-    url="https://github.com/2degrees/django-postgres-delete-cascade",
-    license="BSD (http://dev.2degreesnetwork.com/p/2degrees-license.html)",
+    name='django-postgres-delete-cascade',
+    version=_read_file('VERSION.txt'),
+    description='PostgreSQL engine for Django that supports "ON DELETE CASCADE" at the database level',
+    long_description=_read_file('README.rst'),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Database',
+        ],
+    keywords='django postgres cascade cascading delete foreign key keys',
+    author='2degrees Limited',
+    author_email='2degrees-floss@googlegroups.com',
+    url='https://github.com/2degrees/django-postgres-delete-cascade',
+    license='BSD (http://dev.2degreesnetwork.com/p/2degrees-license.html)',
     packages=find_packages(),
     install_requires=[
-        "Django >= 1.6",
-        "psycopg2 >= 2.5",
+        'Django >= 1.6',
+        'psycopg2 >= 2.5',
         ],
     )
